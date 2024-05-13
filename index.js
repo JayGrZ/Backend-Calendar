b3e98f43b6177205192314af80339b331cdfc22b
Kei <motosjairo12@gmail.com>
2024-05-13 12:34:38 +0200

    cambios nuevos

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
 
+
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
     console.log(`Server corriendo en puerto ${process.env.PORT} `);

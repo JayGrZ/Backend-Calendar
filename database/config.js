dda6ecec499b1a7f1a77a5569123f220d599f35a
Kei <motosjairo12@gmail.com>
2024-06-06 13:00:02 +0200

    comillas DB

--- database/config.js
+++ database/config.js
@@ -5,7 +5,7 @@ const dbConnection = async() => {
 
     try {
 
-        await mongoose.connect( process.env.DB_CNN );
+        await mongoose.connect(process.env.DB_CNN);
 
         console.log('db online')
         
